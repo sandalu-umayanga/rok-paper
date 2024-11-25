7500f6ee4a74b201904e85e76ac67edfdca31855
sandalu umayanga <sandaluumayangaofficial@gmail.com>
2024-11-25 07:35:41 +0530

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['rokmediabucket.s3.amazonaws.com','rokstudentimagebucket.s3.amazonaws.com', 'rokstaffimagebucket.s3.amazonaws.com',"via.placeholder.com"], // Add your S3 bucket domain here
-      },
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'rokmediabucket.s3.amazonaws.com',
+          },
+          {
+              protocol: 'https',
+              hostname: 'rokstudentimagebucket.s3.amazonaws.com',
+          },
+          {
+              protocol: 'https',
+              hostname: 'rokstaffimagebucket.s3.amazonaws.com',
+          },
+          {
+              protocol: 'https',
+              hostname: 'via.placeholder.com',
+          },
+      ],
+  },
 };
 
 export default nextConfig;
